ac244ef59814cb51baee9bd611ec90ae9484ee01
Dohun <kdhhuns2000@gmail.com>
2022-11-07 02:17:45 +0900

    Add constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+// 전공 및 영역을 선택하는 list tile들의 font size
+const double kFilterListFontSize = 20.0;
+// ClipRRect 위젯의 font size
+const double kClipRRectFontSize = 15.0;
+// ClipRRect border radius
+const double kClipRRectBorderRadius = 30.0;
