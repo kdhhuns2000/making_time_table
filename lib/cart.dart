391e67689cea4695efa1fc147a951b9104bbadd2
Dohun <kdhhuns2000@gmail.com>
2022-11-06 17:37:29 +0900

    Add cart list page

---
+++ lib/cart.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+List<dynamic> cartList = [];
+
+// class Cart extends StatefulWidget {
+//   const Cart({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Cart> createState() => _CartState();
+// }
+//
+// class _CartState extends State<Cart> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.separated(
+//       itemCount: cartList.length,
+//       itemBuilder: (BuildContext context, int index) {},
+//       separatorBuilder: (BuildContext context, int index) => const Divider(thickness: 1.0),
+//     );
+//   }
+// }
