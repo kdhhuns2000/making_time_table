b51e33538622666de19a46f267c3af0359f36a1d
Dohun <kdhhuns2000@gmail.com>
2022-11-09 01:56:57 +0900

    Add page view dot indicator

--- lib/table.dart
+++ lib/table.dart
@@ -1,33 +1,83 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
 
 List week = ['월', '화', '수', '목', '금'];
 var kColumnLength = 22;
 
 class TableWidget extends StatefulWidget {
-  const TableWidget({Key? key}) : super(key: key);
+  TableWidget({
+    Key? key,
+    this.cartList,
+  }) : super(key: key);
+  List<dynamic>? cartList;
 
   @override
   State<TableWidget> createState() => _TableWidgetState();
 }
 
 class _TableWidgetState extends State<TableWidget> {
+
+  late int _selectedPage;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      initialPage: _selectedPage = 0,
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: kColumnLength / 2 * kBoxSize + kColumnLength,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(12),
+    return Column(
+      children: [
+        Expanded(
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return Center(
+                child: Container(
+                  height: kColumnLength / 2 * kBoxSize + kColumnLength,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      buildTimeColumn(),
+                      for (int i = 0; i < 5; i++) ...buildDayColumn(i),
+                    ],
+                  ),
+                ),
+              );
+            },
+            onPageChanged: (int index) {
+              setState(() {
+                _selectedPage = index;
+              });
+            },
+          ),
         ),
-        child: Row(
-          children: [
-            buildTimeColumn(),
-            for (int i = 0; i < 5; i++) ...buildDayColumn(i),
-          ],
+        Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: PageViewDotIndicator(
+            currentItem: _selectedPage,
+            count: 3,
+            unselectedColor: Colors.black26,
+            selectedColor: Colors.blue,
+            size: const Size(12, 12),
+            unselectedSize: const Size(8, 8),
+            duration: const Duration(milliseconds: 200),
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            alignment: Alignment.topCenter,
+            fadeEdges: false,
+          ),
         ),
-      ),
+      ],
     );
   }
 
@@ -40,7 +90,7 @@ class _TableWidgetState extends State<TableWidget> {
           ),
           ...List.generate(
             kColumnLength,
-            (index) {
+                (index) {
               if (index % 2 == 0) {
                 return const Divider(
                   color: Colors.grey,
@@ -68,7 +118,6 @@ class _TableWidgetState extends State<TableWidget> {
         flex: 4,
         child: Stack(
           children: <Widget>[
-
             Positioned(
               child: Container(
                 color: Colors.blue,
@@ -92,7 +141,7 @@ class _TableWidgetState extends State<TableWidget> {
                 ),
                 ...List.generate(
                   kColumnLength,
-                  (index) {
+                      (index) {
                     if (index % 2 == 0) {
                       return const Divider(
                         color: Colors.grey,
