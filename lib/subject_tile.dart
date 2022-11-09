91905d44ea8f3279aa31a214eb7fce3e9ed2169d
Dohun <kdhhuns2000@gmail.com>
2022-11-10 03:29:59 +0900

    Add flutter toast and update subject picker

--- lib/subject_tile.dart
+++ lib/subject_tile.dart
@@ -1,14 +1,15 @@
+import 'cart.dart';
+import 'constants.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'cart.dart';
-
-List<bool?> _isChecked = List.filled(100, false);
+import 'package:fluttertoast/fluttertoast.dart';
 
 class SubjectTile extends StatefulWidget {
   final String majorFieldRange; // 전공/영역 매칭
   final String? searchText; // 검색어 매칭
   const SubjectTile({Key? key, this.searchText, required this.majorFieldRange}) : super(key: key);
+
   @override
   State<SubjectTile> createState() => _SubjectTileState();
 }
@@ -20,6 +21,15 @@ class _SubjectTileState extends State<SubjectTile> {
     return map;
   }
 
+  late FToast fToast;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     String? searchText = widget.searchText;
@@ -28,7 +38,7 @@ class _SubjectTileState extends State<SubjectTile> {
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         //해당 부분은 data를 아직 받아 오지 못했을 때 실행되는 부분
         if (snapshot.hasData == false) {
-          return Center(
+          return const Center(
             child: Text(
               '전공/영역을 선택해주세요',
               style: TextStyle(
@@ -44,7 +54,7 @@ class _SubjectTileState extends State<SubjectTile> {
             padding: const EdgeInsets.all(8.0),
             child: Text(
               'Error: ${snapshot.error}', // 에러명을 텍스트에 뿌려줌
-              style: TextStyle(fontSize: 15),
+              style: const TextStyle(fontSize: 15),
             ),
           );
         }
@@ -57,42 +67,110 @@ class _SubjectTileState extends State<SubjectTile> {
             itemCount: snapshot.data[0]['subject'].length,
             itemBuilder: (BuildContext context, int index) {
               var subjectData = snapshot.data[0]['subject'][index];
-              return CheckboxListTile(
-                title: Text.rich(
-                  TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: '${subjectData['name'].toString()}\n',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
+
+              return Row(
+                children: [
+                  Expanded(
+                    child: ListTile(
+                      title: Text.rich(
+                        TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: '${subjectData['name'].toString()}\n',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '${subjectData['professor'].toString()}\n',
+                            )
+                          ],
                         ),
                       ),
-                      TextSpan(
-                        text: '${subjectData['professor'].toString()}\n',
-                      )
-                    ],
+                      subtitle: Text(
+                          '${subjectData['time'].toString()}\n${subjectData['place']}\n${subjectData['type']} ${subjectData['credit']}학점 ${subjectData['id']}'),
+                    ),
                   ),
-                ),
-                subtitle: Text(
-                    '${subjectData['time'].toString()}\n${subjectData['place']}\n${subjectData['type']} ${subjectData['credit']}학점 ${subjectData['id']}'),
-                value: _isChecked[index],
-                onChanged: (bool? value) {
-                  if (value == true) {
-                    cartList.add(subjectData);
-                  } else if (value == false) {
-                    cartList.removeWhere((element) => element['id'] == subjectData['id']);
-                  }
-                  print(cartList);
-                  setState(() {
-                    _isChecked[index] = value;
-                  });
-                },
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: Colors.black12,
+                          textStyle: const TextStyle(fontSize: kClipRRectFontSize),
+                        ),
+                        onPressed: () {
+                          if (cartList.indexWhere((element) => element['id'] == subjectData['id']) != -1) {
+                            _showToast('이미 담은 과목입니다');
+                          } else {
+                            cartList.add(subjectData);
+                          }
+                        },
+                        child: const Text('담기'),
+                      ),
+                    ),
+                  ),
+                ],
               );
+
+              /*
+              Checkbox 방식의 장바구니 담기
+               */
+              // return CheckboxListTile(
+              //   title: Text.rich(
+              //     TextSpan(
+              //       children: <TextSpan>[
+              //         TextSpan(
+              //           text: '${subjectData['name'].toString()}\n',
+              //           style: const TextStyle(
+              //             fontWeight: FontWeight.bold,
+              //           ),
+              //         ),
+              //         TextSpan(
+              //           text: '${subjectData['professor'].toString()}\n',
+              //         )
+              //       ],
+              //     ),
+              //   ),
+              //   subtitle: Text(
+              //       '${subjectData['time'].toString()}\n${subjectData['place']}\n${subjectData['type']} ${subjectData['credit']}학점 ${subjectData['id']}'),
+              //   value: _isChecked[index],
+              //   onChanged: (bool? value) {
+              //     if (value == true) {
+              //       cartList.add(subjectData);
+              //     } else if (value == false) {
+              //       cartList.removeWhere((element) => element['id'] == subjectData['id']);
+              //     }
+              //     print(cartList);
+              //     setState(() {
+              //       _isChecked[index] = value;
+              //     });
+              //   },
+              // );
             },
             separatorBuilder: (BuildContext context, int index) => const Divider(thickness: 1.0),
           );
         }
       },
+    );
+  }
+
+  _showToast(String message) {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.grey[200],
+      ),
+      child: Text(message),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: const Duration(seconds: 2),
     );
   }
 }
