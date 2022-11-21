25d1dae0ebd1f45fb31bf5c506bc5f4efd908928
Dohun <kdhhuns2000@gmail.com>
2022-11-21 11:45:01 +0900

    Add exit function when back button pressed twice

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'cart.dart';
 import 'constants.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'search.dart';
 import 'table.dart';
 
@@ -21,7 +22,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     Search(),
     Cart(),
   ];
-
+  DateTime? currentBackPressTime;
   late TabController _tabController;
 
   @override
@@ -53,70 +54,92 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
-        child: Scaffold(
-          body: _widgetList[selectedIndex.value], // 위젯 여러개 넣을 자리
-          bottomNavigationBar: ConvexAppBar(
-            backgroundColor: kDefaultAppColor,
-            items: const [
-              TabItem(icon: Icons.schedule, title: '시간표'),
-              TabItem(icon: Icons.home, title: '홈'),
-              TabItem(icon: Icons.shopping_cart, title: '장바구니')
-            ],
-            controller: _tabController,
-            onTap: _onItemTapped,
+        child: WillPopScope(
+          onWillPop: () async {
+            bool result = onWillPop();
+            return await Future.value(result);
+          },
+          child: Scaffold(
+            body: _widgetList[selectedIndex.value], // 위젯 여러개 넣을 자리
+            bottomNavigationBar: ConvexAppBar(
+              backgroundColor: kDefaultAppColor,
+              items: const [
+                TabItem(icon: Icons.schedule, title: '시간표'),
+                TabItem(icon: Icons.home, title: '홈'),
+                TabItem(icon: Icons.shopping_cart, title: '장바구니')
+              ],
+              controller: _tabController,
+              onTap: _onItemTapped,
+            ),
+            // bottomNavigationBar: BottomNavigationBar(
+            //   selectedLabelStyle: TextStyle(fontFamily: kDefaultFont, fontSize: 15.0),
+            //   unselectedLabelStyle: TextStyle(fontFamily: kDefaultFont, fontSize: 15.0),
+            //   items: <BottomNavigationBarItem>[
+            //     BottomNavigationBarItem(
+            //       icon: Icon(Icons.schedule),
+            //       label: '시간표',
+            //     ),
+            //     BottomNavigationBarItem(
+            //       icon: Icon(Icons.home),
+            //       label: '홈',
+            //     ),
+            //     BottomNavigationBarItem(
+            //       icon: Stack(
+            //         children: <Widget>[
+            //           Icon(Icons.notifications),
+            //           Visibility(
+            //             visible: _visibility(),
+            //             child: Positioned(
+            //               right: 0,
+            //               child: Container(
+            //                 padding: EdgeInsets.all(1),
+            //                 decoration: BoxDecoration(
+            //                   color: Colors.red,
+            //                   borderRadius: BorderRadius.circular(6),
+            //                 ),
+            //                 constraints: BoxConstraints(
+            //                   minWidth: 12,
+            //                   minHeight: 12,
+            //                 ),
+            //                 child: Text(
+            //                   cartList.length.toString(),
+            //                   style: TextStyle(
+            //                     color: Colors.white,
+            //                     fontSize: 8,
+            //                   ),
+            //                   textAlign: TextAlign.center,
+            //                 ),
+            //               ),
+            //             ),
+            //           )
+            //         ],
+            //       ),
+            //       label: '장바구니',
+            //     ),
+            //   ],
+            //   currentIndex: selectedIndex.value,
+            //   selectedItemColor: kDefaultAppColor,
+            //   onTap: _onItemTapped,
+            // ),
           ),
-          // bottomNavigationBar: BottomNavigationBar(
-          //   selectedLabelStyle: TextStyle(fontFamily: kDefaultFont, fontSize: 15.0),
-          //   unselectedLabelStyle: TextStyle(fontFamily: kDefaultFont, fontSize: 15.0),
-          //   items: <BottomNavigationBarItem>[
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.schedule),
-          //       label: '시간표',
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.home),
-          //       label: '홈',
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Stack(
-          //         children: <Widget>[
-          //           Icon(Icons.notifications),
-          //           Visibility(
-          //             visible: _visibility(),
-          //             child: Positioned(
-          //               right: 0,
-          //               child: Container(
-          //                 padding: EdgeInsets.all(1),
-          //                 decoration: BoxDecoration(
-          //                   color: Colors.red,
-          //                   borderRadius: BorderRadius.circular(6),
-          //                 ),
-          //                 constraints: BoxConstraints(
-          //                   minWidth: 12,
-          //                   minHeight: 12,
-          //                 ),
-          //                 child: Text(
-          //                   cartList.length.toString(),
-          //                   style: TextStyle(
-          //                     color: Colors.white,
-          //                     fontSize: 8,
-          //                   ),
-          //                   textAlign: TextAlign.center,
-          //                 ),
-          //               ),
-          //             ),
-          //           )
-          //         ],
-          //       ),
-          //       label: '장바구니',
-          //     ),
-          //   ],
-          //   currentIndex: selectedIndex.value,
-          //   selectedItemColor: kDefaultAppColor,
-          //   onTap: _onItemTapped,
-          // ),
         ),
       ),
     );
+  }
+
+  onWillPop() {
+    DateTime now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > const Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      Fluttertoast.showToast(
+          msg: "'뒤로' 버튼을 한번 더 누르시면 종료됩니다.",
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: const Color(0xff6E6E6E),
+          fontSize: 20,
+          toastLength: Toast.LENGTH_SHORT);
+      return false;
+    }
+    return true;
   }
 }
